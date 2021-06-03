eb8537e7c96e840ace38d86e3e8f1e06e310b399
imvikki <41563822+imvikki@users.noreply.github.com>
2021-06-04 02:02:45 +0530

    Delete hotelautomation directory

--- hotelautomation/src/main/java/it/example/hotelautomation/interfaces/exception/HotelAutomationException.java
+++
@@ -1,9 +0,0 @@
-package it.example.hotelautomation.interfaces.exception;
-
-public class HotelAutomationException extends RuntimeException {
-
-    public HotelAutomationException(String message) {
-        super(message);
-    }
-
-}
